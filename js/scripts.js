ad7f8aad77079a74a50798b47db56e30b3de3612
RenanMacielLuna <renanluna09@gmail.com>
2024-07-28 11:59:47 -0400

    Deploy commit

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,30 @@ function enableButton() {
   let name = document.forms['contactForm']['name'].value;
   let email = document.forms['contactForm']['email'].value;
   let comment = document.forms['contactForm']['comment'].value;
+  if (name === '') {
+    document.getElementById('invalid-feedback-name').hidden = false;
+  } else {
+    document.getElementById('invalid-feedback-name').hidden = true;
+  }
+  if (email === '') {
+    document.getElementById('invalid-feedback-email').hidden = false;
+  } else {
+    document.getElementById('invalid-feedback-email').hidden = true;
+  }
+  if (comment === '') {
+    document.getElementById('invalid-feedback-comment').hidden = false;
+  } else {
+    document.getElementById('invalid-feedback-comment').hidden = true;
+  }
   if (name !== '' && email !== '' && comment !== '') {
     document.getElementById('my-button').disabled = false;
   } else {
     document.getElementById('my-button').disabled = true;
   }
+}
+
+function enableButtonIssue() {}
+
+function showAlertIssue() {
+  alert('We will be in contact with you soon. Please, check your e-mail.');
 }
