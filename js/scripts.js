7d9f7282ca58ba5dcd212d68863fe721bf8f184c
RenanMacielLuna <renanluna09@gmail.com>
2024-07-28 15:41:20 -0400

    Minor fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function showAlert() {
-  alert('Comment Added.');
+  alert('Comment Added. Wait some minutes for your comment to be published.');
 }
 
 function enableButton() {
@@ -28,7 +28,32 @@ function enableButton() {
   }
 }
 
-function enableButtonIssue() {}
+function enableButtonIssue() {
+  let name = document.forms['issueForm']['name'].value;
+  let email = document.forms['issueForm']['email'].value;
+  let issue = document.forms['issueForm']['issue'].value;
+
+  if (name === '') {
+    document.getElementById('invalid-issue-name').hidden = false;
+  } else {
+    document.getElementById('invalid-issue-name').hidden = true;
+  }
+  if (email === '') {
+    document.getElementById('invalid-issue-email').hidden = false;
+  } else {
+    document.getElementById('invalid-issue-email').hidden = true;
+  }
+  if (issue === '') {
+    document.getElementById('invalid-issue-issue').hidden = false;
+  } else {
+    document.getElementById('invalid-issue-issue').hidden = true;
+  }
+  if (name !== '' && email !== '' && issue !== '') {
+    document.getElementById('my-button-issue').disabled = false;
+  } else {
+    document.getElementById('my-button-issue').disabled = true;
+  }
+}
 
 function showAlertIssue() {
   alert('We will be in contact with you soon. Please, check your e-mail.');
